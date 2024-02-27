dade619fa3a096c1fd9e571509333dc55d1a67ef
aymenbouguerra95 <aymen.bouguerra.95@gmail.com>
2024-02-27 15:34:27 +0100

    change function.h

--- fonction-bienvenue.h
+++ fonction-bienvenue.h
@@ -1,5 +1,16 @@
 #ifndef FONCTION_BIENVENUE_H
 #define FONCTION_BIENVENUE_H
+
 #include <string>
-void afficherBienvenue(std::string message="Bienvenue le monde !");
+
+void afficherBienvenue(std::string message = "Bienvenue le monde !", int nbAffichage = 1);
+
 #endif // FONCTION_BIENVENUE_H
+#include "fonction-bienvenue.h"
+#include <iostream>
+
+void afficherBienvenue(std::string message, int nbAffichage) {
+    for (int i = 0; i < nbAffichage; ++i) {
+        std::cout << message << std::endl;
+    }
+}
